25c0f7767415f5f5533075a257d9df3f1a8ab7b2
Abu Bakar <76771529+Abu0978123@users.noreply.github.com>
2024-01-05 10:39:17 +0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+// jquery events
+jQuery.noConflict();
+
+jQuery(document).ready(function($){
+
+    // $("#fadein").click(function(){
+    //     $("#img").fadeIn(2000, function(){
+    //         console.log('fadeIn is working')
+    //     })
+    // })
+    // $("#fadeout").click(function(){
+    //     $("#img").fadeOut(2000, function(){
+    //         console.log('fadeOut is working')
+    //     })
+    // })
+
+    // $("#fadetoggle").click(function(){
+    //     $("#img").fadeToggle(2000, function(){
+    //         console.log('toggle is working')
+    //     })
+    // })
+    // for focus the input 
+    // $("#focus").focus(function(){
+    //     console.log('focus here');
+    // })
+
+    // // for blur the input 
+    // $("#blur").blur(function(){
+    //     console.log("blur here")
+    // })
+
+    // // for keyup in the input
+    // $("#keyup").keyup(function(){
+    //     console.log("keyup in the input", keyup.value)
+    // })
+})
+
+
+
+// jQuery(document).ready(function($){
+
+//     // double clicked
+//     $('#double').dblclick(function (){
+//         alert("double clicked")
+//     })
+     
+//     // mouseenter
+//     $('#mouseenter').mouseenter(function(){
+//         alert('mouseentered')
+//     })
+
+//     // mouseleave
+//     $('#mouseleave').mouseleave(function(){
+//        alert('mouseleaved')
+//     })
+// })
+
+// $("p").click(function(){
+//     $(this).hide();
+//   });
+ 
+// jQuery.noConflict();
+// jQuery(document).ready(function($){
+//     $('button').click(function(){
+//         // alert("noConflict is working")
+//         $("p").toggle();
+//     })
+// })
+ 
+ //       $(document).ready(function(){
+    //    $("button").click(function(){
+    //       $("p").toggle();
+    //    })
+    //   })
+
+    // $(document).ready(function(){
+        // $("button").click(function(){
+        //    $("p").toggle(1000);
+          
+        // })
+ //        $("#p1").mouseenter(function(){
+ //   alert("You entered p1!");
+ // });
+    //    })
+ 
+//  console.log($)
+//  console.log(jQuery) 
