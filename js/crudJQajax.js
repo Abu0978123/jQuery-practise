7bc71c7297b6c9a20d4818d2b39792bbc177c3c2
Abu Bakar <76771529+Abu0978123@users.noreply.github.com>
2024-01-05 22:24:17 +0500

    method get added

--- js/crudJQajax.js
+++ js/crudJQajax.js
@@ -1,7 +1,37 @@
 jQuery.noConflict();
 
 jQuery(document).ready(function ($) {
-  // getting data
+     
+    // getting data from api
+    output = "";
+    function showdata(){
+        $.ajax({
+            url: "https://6572a8f0192318b7db40788e.mockapi.io/rtk-query",
+            method: "GET",
+            dataType: "json",
+            success: function(data){
+                if(data){
+                    x = data;
+                } else {
+                    x = "";
+                }
+                for(i = 0; i < x.length; i++){
+                    output  +=
+                    "<tr><td>" +
+                    x[i].id +
+                    "</td><td>" +
+                    x[i].name +
+                    "</td><td>" +
+                    x[i].email +
+                    "</td><td> <button class='btn btn-warning btn-sm btn-edit'>Edit</button> <button class='btn btn-danger btn-sm btn-edit'>Delete </button> </td></tr>";
+                }
+                $("#tbody").html(output);
+            }
+        });
+    }
+showdata()
+
+  // posting data
   $("button").click(function (e) {
     e.preventDefault();
     console.log("button clicked");
@@ -21,6 +51,7 @@ jQuery(document).ready(function ($) {
         successRes = "Data added successfully:"
         $("#msg").html(successRes)
         $("#myform")[0].reset();
+        // showdata();
     },
     error: function (result) {
         alert("Error");
